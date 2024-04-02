efd7ba37804fba93c37aa26c06e0d8c4e3108262
Marco Barboza <Marco.Barboza@smithgroup.com>
2024-04-01 21:21:25 -0700

    Add Room model

--- meetings/models.py
+++ meetings/models.py
@@ -10,3 +10,12 @@ class Meeting(models.Model):
     
     def __str__(self):
         return f"{self.title} at {self.start_time} on {self.date}"
+    
+
+class Room(models.Model):
+    name = models.CharField(max_length=50)
+    floor = models.IntegerField()
+    room_number = models.IntegerField()
+    
+    def __str__(self):
+        return f"{self.name}: room {self.room_number} on floor {self.floor}"
