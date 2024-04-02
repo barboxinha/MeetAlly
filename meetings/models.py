f6bd8ca104c34bf409b816651fc8d0ca6b456bed
Marco Barboza <Marco.Barboza@smithgroup.com>
2024-04-01 21:05:58 -0700

    Add new time and duration fields to Meeting model

--- meetings/models.py
+++ meetings/models.py
@@ -1,5 +1,9 @@
+from datetime import time
+
 from django.db import models
 
 class Meeting(models.Model):
     title = models.CharField(max_length=200)
     date = models.DateField()
+    start_time = models.TimeField(default=time(9))
+    duration = models.IntegerField(default=1)
