e3a5c459fdccf7068bb853b0748b3fc39c7d03e4
Marco Barboza <Marco.Barboza@smithgroup.com>
2024-04-02 19:32:37 -0700

    Return error 404 if meeting not found

--- meetings/views.py
+++ meetings/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from meetings.models import Meeting
 
 # View function for detail of a meeting
 def detail(request, id):
-    meeting = Meeting.objects.get(pk=id)
+    meeting = get_object_or_404(Meeting, pk=id)
     return render(request, "meetings/detail.html", {"meeting": meeting})
