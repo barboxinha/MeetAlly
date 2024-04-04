dcc07cee5d62c1425b0fa657eeaffa4c8cedbca9
Marco Barboza <Marco.Barboza@smithgroup.com>
2024-04-03 20:06:16 -0700

    Include meeting app urls in global urls

--- MeetAlly/urls.py
+++ MeetAlly/urls.py
@@ -15,15 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 from website.views import welcome, about
-from meetings.views import detail, room_list
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', welcome, name="welcome"),
     path('about', about),
-    path('meetings/<int:id>', detail, name="detail"),
-    path('rooms', room_list, name="rooms"),
+    path('meetings/', include('meetings.urls'))
 ]
---
+++ meetings/urls.py
@@ -0,0 +1,11 @@
+"""
+URLs for the meetings app.
+"""
+from django.urls import path
+
+from meetings import views
+
+urlpatterns = [
+    path('<int:id>', views.detail, name="detail"),
+    path('rooms', views.room_list, name="rooms")
+]
