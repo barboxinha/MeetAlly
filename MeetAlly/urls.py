ebdbb6e20b53006a02e9e737749957c83a4dc3bf
Marco Barboza <Marco.Barboza@smithgroup.com>
2024-04-03 18:03:42 -0700

    Add Home link to detail template

--- MeetAlly/urls.py
+++ MeetAlly/urls.py
@@ -22,7 +22,7 @@ from meetings.views import detail
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', welcome),
+    path('', welcome, name="welcome"),
     path('about/', about),
     path('meetings/<int:id>', detail, name="detail"),
 ]
