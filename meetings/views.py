1d48224c49f754b33803e3f72b63503b826a4a9e
Marco Barboza <Marco.Barboza@smithgroup.com>
2024-04-04 18:49:24 -0700

    Create new meeting form

--- meetings/views.py
+++ meetings/views.py
@@ -1,12 +1,24 @@
 from django.shortcuts import render, get_object_or_404
+from django.forms import modelform_factory
 
 from meetings.models import Meeting, Room
 
-# View function for detail of a meeting
+
+# View functions
 def detail(request, id):
     meeting = get_object_or_404(Meeting, pk=id)
     return render(request, "meetings/detail.html", {"meeting": meeting})
 
+
 def room_list(request):
     rooms = Room.objects.all()
     return render(request, "meetings/room_list.html", {"rooms": rooms})
+
+
+# This is a new class generated from your model by the modelform_factory function.
+# To exclude specific fields from the form, pass them as a list to the exclude parameter.
+MeetingForm = modelform_factory(Meeting, exclude=[])
+
+def new(request):
+    form = MeetingForm()
+    return render(request, "meetings/new.html", {"form": form})
